1ce2f0980c73a90a2ee998ab91a9bd865e90d841
banea9 <banea9@gmail.com>
2020-04-30 22:03:29 +0200

    Updates

---
+++ precache-manifest.7f7b756028eea39079d0b05382035103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcde5b290a8a506db90dbf17540a323",
+    "url": "/meal-finder-react/index.html"
+  },
+  {
+    "revision": "140bdea184989a4408e6",
+    "url": "/meal-finder-react/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "f238a9b836a8c50397c7",
+    "url": "/meal-finder-react/static/css/main.08c4aad1.chunk.css"
+  },
+  {
+    "revision": "140bdea184989a4408e6",
+    "url": "/meal-finder-react/static/js/2.919f3389.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meal-finder-react/static/js/2.919f3389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f238a9b836a8c50397c7",
+    "url": "/meal-finder-react/static/js/main.dd11b19e.chunk.js"
+  },
+  {
+    "revision": "03d7af172b95e4dc7c7c",
+    "url": "/meal-finder-react/static/js/runtime-main.34c5a616.js"
+  }
+]);
