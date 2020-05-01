672e2e6f571741fc5875f2e9ddde91e83b87e080
banea9 <banea9@gmail.com>
2020-05-01 18:37:05 +0200

    nwe changes

--- src/components/MealDetails/MealDetails.js
+++ src/components/MealDetails/MealDetails.js
@@ -4,22 +4,23 @@ import Ingredients from "../Ingredients/Ingredients";
 const MealDetails = ({ mealDetails }) => {
   return (
     <div className="tc meal-desc">
-      <h1 className="f2">{mealDetails.name}</h1>
+      <h1 className="f2 tc">{mealDetails.name}</h1>
       <img src={mealDetails.photoSrc} width="350px" height="350px" alt="asd" />
       <div className="desc tc f3 ph4 pv3 br3 ba b--dashed bw2">
         <h2>{mealDetails.category}</h2>
         <h4>{mealDetails.area}</h4>
-        <a
-          href={mealDetails.video}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ textDecoration: "none", color: "#fff" }}
-        >
-          Video
-        </a>
       </div>
       <div className="instructions f4">{mealDetails.instructions}</div>
       <Ingredients ingredients={mealDetails.ingredients} />
+      <a
+        href={mealDetails.video}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="f3 mv3"
+        style={{ textDecoration: "none", color: "#bd8532" }}
+      >
+        Video preparation
+      </a>
     </div>
   );
 };
